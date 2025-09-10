9a752fe22152d7e031e32960edd7ffca8e006fb9
Vitor Nirschl <vitornirschl@gmail.com>
2025-09-10 19:24:06 -0300

    Added Stacks datatype; Added docstrings to Fractions.

--- README.md
+++ README.md
@@ -11,7 +11,30 @@ Trata-se ainda de uma boa oportunidade para colocar aplicar boas práticas de pr
 - [ ] Conjuntos
 - [ ] Vetores
 - [ ] Matrizes
-- [ ] Frações
+- [x] Frações
 - [ ] Polinômios
-- [ ] Pilhas (Stacks)
+- [x] Pilhas (Stacks)
 - [ ] Filas (Queues)
+
+#### Frações
+Uma instância da Fraction recebe um numerador e um denominador, ambos inteiros, os quais são simplificados quando houver fator comum. O gerenciamento de exceções garante o funcionamento do módulo conforme planejado.
+
+Foi implementada a sobrecarga dos operadores de soma, subtração, multiplicação, divisão, divisão inteira, resto, exponenciação, valor absoluto, além dos operadores de comparação e de conversão para outros tipos numéricos. Também foram implementadas as representações de string e abstrata da classe.
+
+_TODO: Módulo de testagem da classe de frações_
+
+#### Pilhas
+A estrutura de dados Stack (pilha) obedece a lógica LIFO/FILO (Last Fn, First Out ou First In, Last Out) - em português, o último a entrar é o primeiro a sair e o primeiro a entrar é o último a sair. Como numa pilha de pratos, o último item a ser adicionado na pilha é o primeiro a ser retirado e vice-versa.
+
+Foram implementadas duas formas de pilhas: uma com tamanho flexível, tendo sua alocação de espaço na memória aumentada conforme necessário, e outra com tamanho fixo. A primeira tem um tempo de execução maior em decorrência da alocação dinâmica da memória, diferentemente da segunda, que é mais ágil. 
+
+Foram implementados os métodos
+
+- is_empty() : verifica se a pilha está vazia;
+- push() : adiciona um ítem ao topo da pilha;
+- pop() : remove o último item da pilha;
+- top() : mostra o último ítem da pilha.
+
+Além disso, para as pilhas de tamanho fixo, foi implementado o método _expand(max_length)_ que aumenta a pilha até o tamanho max_length. Também foram implementadas as representações de string e abstrata das classes de pilhas e a sobrecarga do operador len.
+
+ _TODO: Módulo de testagem da classe de pilhas_
--- abstract_data_types/fractions.py
+++ abstract_data_types/fractions.py
@@ -1,15 +1,43 @@
 '''
-Implement the Fraction Abstract Data Type.
+Implements the Fraction Abstract Data Type.
+
+Classes
+-------
+Fraction
+    A fraction in its simplest form.
 '''
 
 from math import gcd
 
 class Fraction:
     '''
-    The simplest form of a fraction, obbeying 
-    regular algebra rules for basic math operations.
+    A Fraction in its simplest form.
+
+    Attributes
+    ----------
+    numerator : int
+        The numerator of the simplified fraction.
+    denominator : int
+        The denominator of the simplified fraction.
     '''
     def __init__(self, numerator, denominator=1):
+        '''
+        Initializes a Fraction instance.
+
+        Parameters
+        ----------
+        numerator : int
+            The numerator of the fraction.
+        denominator : int, optional
+            The denominator of the fraction (default = 1).
+
+        Raises
+        ------
+        TypeError
+            If the numerator or the denominator aren't integers.
+        ZeroDivisionError
+            If the denominator is set to zero.  
+        '''
         # Exception handling
         if not isinstance(numerator, int) or not isinstance(denominator, int):
             raise TypeError("The numerator and the denominator must be integer numbers.")
@@ -22,15 +50,32 @@ class Fraction:
         else: signal = -1
 
         numerator, denominator = abs(numerator), abs(denominator)
+        # Using greatest common divisor to simplify the fraction.
         factor = gcd(numerator, denominator)
 
         self.numerator = signal * numerator // factor
         self.denominator = denominator // factor
 
     def __repr__(self):
+        '''
+        The abstract representation of an instance of Fraction
+
+        Returns
+        -------
+        str
+            Fraction({self.numerator}, {self.denominator})
+        '''
         return f"Fraction({self.numerator}, {self.denominator})"
 
     def __str__(self):
+        '''
+        The string object of an instance of Fraction.
+
+        Returns
+        -------
+        str
+            A visual representation of a simplified fraction.
+        '''
         if self.denominator == 1:
             return f"{self.numerator}"
         if self.numerator == 0:
@@ -38,6 +83,24 @@ class Fraction:
         return f"{self.numerator} / {self.denominator}"
 
     def __add__(self, other):
+        '''
+        The sum of two instances of Fraction.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to sum to the current fraction.
+
+        Returns
+        -------
+        Fraction
+            The sum of the two fractions.
+        
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -49,6 +112,24 @@ class Fraction:
         return Fraction(numerator, denominator)
 
     def __iadd__(self, other):
+        '''
+        The sum of two instances of Fraction.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to sum to the current fraction.
+
+        Returns
+        -------
+        Fraction
+            The sum of the two fractions.
+        
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -58,6 +139,24 @@ class Fraction:
         return self + other
 
     def __sub__(self, other):
+        '''
+        The subtraction of two instances of Fraction.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to substract from the current fraction.
+        
+        Returns
+        -------
+        Fraction
+            The subtraction of the two fractions.
+        
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -67,6 +166,24 @@ class Fraction:
         return self + Fraction(-1) * other
 
     def __isub__(self, other):
+        '''
+        The subtraction of two instances of Fraction.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to substract from the current fraction.
+        
+        Returns
+        -------
+        Fraction
+            The subtraction of the two fractions.
+        
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -76,6 +193,24 @@ class Fraction:
         return self - other
 
     def __mul__(self, other):
+        '''
+        The multiplication of two instances of Fraction.
+        
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to multiply the current fraction.
+
+        Returns
+        -------
+        Fraction
+            The result of the multiplication.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int. 
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -87,6 +222,24 @@ class Fraction:
         return Fraction(numerator, denominator)
 
     def __imul__(self, other):
+        '''
+        The multiplication of two instances of Fraction.
+        
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to multiply the current fraction.
+
+        Returns
+        -------
+        Fraction
+            The result of the multiplication.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int. 
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -96,6 +249,26 @@ class Fraction:
         return self * other
 
     def __truediv__(self, other):
+        '''
+        The division of two fractions.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to divide the current fraction.
+        
+        Returns
+        -------
+        Fraction
+            The result of the division.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        ZeroDivisionError
+            If other is equal to zero.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -109,6 +282,26 @@ class Fraction:
         return Fraction(numerator, denominator)
 
     def __itruediv__(self, other):
+        '''
+        The division of two fractions.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to divide the current fraction.
+        
+        Returns
+        -------
+        Fraction
+            The result of the division.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        ZeroDivisionError
+            If other is equal to zero.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -120,48 +313,154 @@ class Fraction:
         return self / other
 
     def __floordiv__(self, other):
+        '''
+        The floor division of two fractions.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to divide with remainder the current fraction.
+
+        Returns
+        -------
+        Fraction
+            The result of the floor division.
+        
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        ZeroDivisionError
+            If other is equal to zero.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
         if not isinstance(other, Fraction):
             raise TypeError("You can only divide a fraction by another fraction or by an integer.")
+        if other.numerator == 0:
+            raise ZeroDivisionError("Division by zero is undefined.")
 
         floordiv =  (self.numerator * other.denominator) // (other.numerator * self.denominator)
 
         return Fraction(floordiv)
 
     def __ifloordiv__(self, other):
+        '''
+        The floor division of two fractions.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to divide with remainder the current fraction.
+
+        Returns
+        -------
+        Fraction
+            The result of the floor division.
+        
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        ZeroDivisionError
+            If other is equal to zero.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
         if not isinstance(other, Fraction):
             raise TypeError("You can only divide a fraction by another fraction or by an integer.")
+        if other.numerator == 0:
+            raise ZeroDivisionError("Division by zero is undefined.")
 
         return self // other
 
     def __mod__(self, other):
+        '''
+        The remainder of the floor division of two fractions.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to divide the current fraction.
+        
+        Returns
+        -------
+        Fraction
+            The remainder of the floor division of the current fraction by other.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        ZeroDivisionError
+            If other is equal to zero.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
         if not isinstance(other, Fraction):
             raise TypeError("You can only take the module between a fraction and another " +
             "fraction or a fraction and an integer.")
+        if other.numerator == 0:
+            raise ZeroDivisionError("Division by zero is undefined.")
 
         mod = (self.numerator * other.denominator) % (other.numerator * self.denominator)
 
         return Fraction(mod)
 
     def __imod__(self, other):
+        '''
+        The remainder of the floor division of two fractions.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to divide the current fraction.
+        
+        Returns
+        -------
+        Fraction
+            The remainder of the floor division of the current fraction by other.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        ZeroDivisionError
+            If other is equal to zero.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
         if not isinstance(other, Fraction):
             raise TypeError("You can only take the module between a fraction and another " +
             "fraction or a fraction and an integer.")
+        if other.numerator == 0:
+            raise ZeroDivisionError("Division by zero is undefined.")
 
         return self % other
 
     def __pow__(self, power):
+        '''
+        Raises the fraction to any given power.
+
+        Args
+        ----
+        power : int
+            The power to raise the fraction.
+
+        Returns
+        -------
+        Fraction
+            The result of the exponentiation.
+        
+        Raises
+        ------
+        TypeError
+            If power is not an instance of int.    
+        '''
         # Exception handling
         if not isinstance(power, int):
             raise TypeError("The power must be an integer.")
@@ -175,6 +474,24 @@ class Fraction:
         return Fraction(numerator, denominator)
 
     def __ipow__(self, power):
+        '''
+        Raises the fraction to any given power.
+
+        Args
+        ----
+        power : int
+            The power to raise the fraction.
+
+        Returns
+        -------
+        Fraction
+            The result of the exponentiation.
+        
+        Raises
+        ------
+        TypeError
+            If power is not an instance of int.    
+        '''
          # Exception handling
         if not isinstance(power, int):
             raise TypeError("The power must be an integer.")
@@ -182,6 +499,24 @@ class Fraction:
         return self ** power
 
     def __eq__(self, other):
+        '''
+        Checks if two fractions are equal.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to be compared with the current fraction.
+
+        Returns
+        -------
+        Bool
+            The result of the comparison.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -191,6 +526,24 @@ class Fraction:
         return self.numerator * other.denominator == self.denominator * other.numerator
 
     def __lt__(self, other):
+        '''
+        Checks if the current fraction is smaller than other.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to be compared with the current fraction.
+
+        Returns
+        -------
+        Bool
+            The result of the comparison.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -200,6 +553,24 @@ class Fraction:
         return self.numerator * other.denominator < self.denominator * other.numerator
 
     def __le__(self, other):
+        '''
+        Checks if the current fraction is smaller or equal than other.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to be compared with the current fraction.
+
+        Returns
+        -------
+        Bool
+            The result of the comparison.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -209,6 +580,24 @@ class Fraction:
         return self.numerator * other.denominator <= self.denominator * other.numerator
 
     def __gt__(self, other):
+        '''
+        Checks if the current fraction is greater than other.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to be compared with the current fraction.
+
+        Returns
+        -------
+        Bool
+            The result of the comparison.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -218,6 +607,24 @@ class Fraction:
         return self.numerator * other.denominator > self.denominator * other.numerator
 
     def __ge__(self, other):
+        '''
+        Checks if the current fraction is greater or equal than other.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to be compared with the current fraction.
+
+        Returns
+        -------
+        Bool
+            The result of the comparison.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -227,6 +634,24 @@ class Fraction:
         return self.numerator * other.denominator >= self.denominator * other.numerator
 
     def __ne__(self, other):
+        '''
+        Checks if two fractions are not equal.
+
+        Args
+        ----
+        other : Fraction or int
+            A fraction or integer to be compared with the current fraction.
+
+        Returns
+        -------
+        Bool
+            The result of the comparison.
+
+        Raises
+        ------
+        TypeError
+            If other is not an instance of Fraction or of int.
+        '''
         # Exception handling
         if isinstance(other, int):
             other = Fraction(other)
@@ -236,21 +661,39 @@ class Fraction:
         return not self == other
 
     def __neg__(self):
+        '''
+        Returns the negative of the fraction.
+        '''
         return Fraction(-1) * self
 
     def __pos__(self):
+        '''
+        Returns the positive of the fraction.
+        '''
         return self
 
     def __abs__(self):
+        '''
+        Returns the absolute value of the fraction.
+        '''
         numerator = abs(self.numerator)
         denominator = self.denominator
         return Fraction(numerator, denominator)
 
     def __complex__(self):
+        '''
+        Returns the evaluated fraction as a complex. 
+        '''
         return complex(self.numerator / self.denominator)
 
     def __int__(self):
+        '''
+        Returns the approximation of the evaluated fraction as an integer.
+        '''
         return int(self.numerator / self.denominator)
 
     def __float__(self):
+        '''
+        Returns the evaluated fraction as a float.
+        '''
         return self.numerator / self.denominator
